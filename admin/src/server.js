fc1eae19963201fa15fe6f0ee54e1bac699157a6
Aristote Diasonama <aristotediasonama@yahoo.fr>
2017-05-28 18:59:12 -0400

    remove admin web app

--- admin/src/server.js
+++
@@ -1,5 +0,0 @@
-<span>
-                    {this.state.docked &&
-                    <a onClick={this.toggleDocked} href="#" style={styles.contentHeaderMenuLink}></a>}
-    <span> S-A-B</span>
-                </span>
