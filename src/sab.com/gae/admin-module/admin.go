782b3e705c34ebc58c658facbf78bede22655b5f
Aristote Diasonama <aristotediasonama@yahoo.fr>
2017-05-30 00:24:33 -0400

    Allow Content-Type and Accept header used in the webapp

--- src/sab.com/gae/admin-module/admin.go
+++ src/sab.com/gae/admin-module/admin.go
@@ -10,6 +10,7 @@ func withContext(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		GetContextStore().SetContext(appengine.NewContext(r))
 		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Accept")
 		f(w, r)
 	}
 }
